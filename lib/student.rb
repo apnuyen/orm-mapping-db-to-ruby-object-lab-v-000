10cac390f44bc315db84e068fe6843309a06e7d3
An Nuyen <annuyen@gmail.com>
2018-02-13 17:49:05 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -89,6 +89,16 @@ class Student
   end
 
   def self.first_X_students_in_grade_10(arg)
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE grade = 10
+      LIMIT ?
+    SQL
+
+    DB[:conn].execute(sql, arg).map do |row|
+      self.new_from_db(row)
+    end
   end
 
   def self.first_student_in_grade_10
